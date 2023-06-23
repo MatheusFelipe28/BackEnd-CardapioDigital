a0fdb4add622decb4af418d11deda710afba4c2d
Matheus <matheusfelipegomesfernandes@gmail.com>
2023-06-22 22:25:22 -0300

    Subindo Aplicação

---
+++ src/test/java/com/example/cardapio/CardapioApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+}
